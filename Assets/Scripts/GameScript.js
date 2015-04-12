a3903b6f31a95acd230f265e700dbd3dadca89c2
Eric Lin <ericlin@axiomzen.co>
2015-04-11 17:15:41 -0700

    fix compile errors

--- Assets/Scripts/GameScript.js
+++ Assets/Scripts/GameScript.js
@@ -15,4 +15,35 @@ static class GameState {
 			}
 		}
 	}
+	
+	function monsterTurn() {
+		for(var i : int = 0; i < this.monsters.Length; i++) {
+			for(var j : int = 0; j < this.monsters.Length; j++) {
+				if (monsters[i, j]) {
+					randomMove(monsters[i, j]);
+				}
+			}
+		}
+	}
+	
+	function randomMove(mob : Monster) {
+		
+	}
+	
+	function findAvailablePositions(vec : Vector2) : Array {
+		var resultArr = new Array();
+		if(!map[vec.x - 1, vec.y]) {
+			resultArr.push(new Vector2(vec.x - 1, vec.y));	
+		}
+		if(!map[vec.x + 1, vec.y]) {
+			resultArr.push(new Vector2(vec.x + 1, vec.y));
+		}
+		if(!map[vec.x, vec.y + 1]) {
+			resultArr.push(new Vector2(vec.x, vec.y + 1));
+		}
+		if(!map[vec.x, vec.y - 1]) {
+			resultArr.push(new Vector2(vec.x, vec.y - 1));
+		}
+		return resultArr;
+	}
 }
