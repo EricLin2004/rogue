d8318f7f004fe9d39f48d84fdabb6a3b4aa88f2f
Eric Lin <ericlin@axiomzen.co>
2015-04-12 18:17:47 -0700

    Click monster tile to check health

--- Assets/Scripts/TileClickScript.js
+++ Assets/Scripts/TileClickScript.js
@@ -1,17 +1,23 @@
 #pragma strict
 
 function OnMouseDown() {
+	Debug.Log(CheckMonsterTile(this.gameObject));
 	if (CheckDistance(GameObject.Find("PlayerPiece(Clone)"), this.gameObject)) {
 		var newVec : Vector2;
 		
 		newVec = this.gameObject.transform.position;
 		GameState.move(newVec);
 		GameState.monsterTurn();
-	} else {
-//		this.gameObject.transform.position;	
+	} else if (CheckMonsterTile(this.gameObject)) {
+		var mob = CheckMonsterTile(this.gameObject);
+		mob.healthSlider.value = Mathf.Floor((mob.currentLife * 100) / mob.overallStats.life);
 	}
 }
 
 function CheckDistance(player : GameObject, go : GameObject) {
 	return Mathf.Abs(go.transform.position.x - player.transform.position.x) + Mathf.Abs(go.transform.position.y - player.transform.position.y) == 1;
+}
+
+function CheckMonsterTile(targetTile : GameObject) {
+	return GameState.monsters[targetTile.transform.position.x, targetTile.transform.position.y];
 }
